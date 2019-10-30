1f0ffa322e2bc4fc96c1d98d7fd266e0127bc015
Astraalito <nicolas.asri@epsi.fr>
2019-10-30 16:21:35 +0100

    Réagencement de l'arobréscence

---
+++ script/script.js
@@ -0,0 +1,29 @@
+var config = {
+    type: Phaser.AUTO,
+    width: screen.width,
+    height: screen.height,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('sol1', 'ressources/img/map/base/sol1.png');
+    this.load.image('sol2', 'ressources/img/map/base/sol2.png');
+    this.load.image('ciel', 'ressources/img/map/base/ciel.png');
+}
+
+function create ()
+{
+    this.add.image(960, 540, 'ciel');
+}
+
+function update () 
+{
+
+}
